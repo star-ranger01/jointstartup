139a39a3d018210832304ecfffc7fac06fa453b0
skyranger05 <ian.capri@live.com>
2023-06-21 11:08:45 -0700

    Added three new variable in the smart contract

--- JointSavings.sol
+++ JointSavings.sol
@@ -3,4 +3,7 @@ pragma solidity ^0.5.0;
 contract JointSavings{
 	address public accountOne;
 	address public accountTwo;
+	address public lastToWithdraw;
+	uint public lastWithdrawAmount;
+	uint public contractBalance;
 }
